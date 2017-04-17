dfe84b30b06d8138c64feddd3a15b23c8175baee
PanDingrong0129 <1084288424@qq.com>
2017-04-17 20:55:05 +0800

    Add pages constant source file

---
+++ src/main/java/com/xtu/constant/Pages.java
@@ -0,0 +1,126 @@
+package com.xtu.constant;
+
+/**
+ * Created by Ilovezilian on 2017/4/17.
+ */
+public class Pages {
+    /**
+     * 版权信息
+     */
+    public static final String COPYRIGHT = "copyright";
+    /**
+     * 404错误处理页面
+     */
+    public static final String NOTFOUNDERROR = "error404";
+    /**
+     * 登录失败页面
+     */
+    public static final String LOGINERROR = "errorLogin";
+    /**
+     * 修改密码
+     */
+    public static final String MODIFYPASSWORD = "modifyPassword";
+    /**
+     * 忘记密码页面
+     */
+    public static final String FORGETPASSWORD = "forgetPassword";
+    /**
+     * 主页
+     */
+    public static final String INDEX = "index";
+    /**
+     * 登录页面
+     */
+    public static final String LOGIN = "login";
+    /**
+     * 导航栏
+     */
+    public static final String NAVIGATION = "navigation";
+    /**
+     * 题库主页
+     */
+    public static final String PROBLEMS = "problems";
+    /**
+     * 题目详细信息
+     */
+    public static final String PRODETAIL = "proDetail";
+    /**
+     * 做题排行榜界面
+     */
+    public static final String RANKLIST = "ranklist";
+    /**
+     * 注册用户页面
+     */
+    public static final String REGISTER = "register";
+    /**
+     * 提交题目界面
+     */
+    public static final String SUBMIT = "submit";
+    /**
+     * 框架页面
+     */
+    public static final String MAIN = "main";
+    /**
+     * 个人信息
+     */
+    public static final String INFO = "info";
+
+    /**
+     * 学生目录
+     */
+    public static final String STUDENT = "student";
+    /**
+     * 修改个人信息页面
+     */
+    public static final String STUINFO = "stuInfo";
+    /**
+     * 修改密码或个人信息成功后跳转的界面
+     */
+    public static final String SUCCESS = "success";
+
+    /**
+     * 老师目录
+     */
+    public static final String TEACHER = "teacher";
+    /**
+     * 修改个人信息
+     */
+    public static final String TEACHERINFO = "teacherInfo";
+    /**
+     * 查看所有学生
+     */
+    public static final String SHOWSTUDENT = "showStudent";
+    /**
+     * 添加学生
+     */
+    public static final String ADDSTUDENT = "addStudent";
+    /**
+     * 添加题目
+     */
+    public static final String ADDPROBLEM = "addProblem";
+    /**
+     * 题目管理
+     */
+    public static final String PROMANAGER = "proManager";
+    /**
+     * 管理员目录
+     */
+    public static final String ADMIN = "admin";
+
+    /**
+     * 比赛目录、比赛页面主页
+     */
+    public static final String TEST = "test";
+    /**
+     * 所有比赛
+     */
+    public static final String ALLTEST = "allTest";
+    /**
+     * 创建比赛
+     */
+    public static final String CREATETEST = "createTest";
+    /**
+     * 加入比赛
+     */
+    public static final String JOINTEST = "joinTest";
+}
