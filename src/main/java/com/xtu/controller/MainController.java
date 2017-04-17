51cc55205081761140aa62035bea536bfe440359
PanDingrong0129 <1084288424@qq.com>
2017-04-17 14:22:29 +0800

    Add post method to index request

--- src/main/java/com/xtu/controller/MainController.java
+++ src/main/java/com/xtu/controller/MainController.java
@@ -4,6 +4,7 @@ import com.xtu.tools.OUT;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestPart;
 
 /**
  * Created by Ilovezilian on 2017/4/13.
@@ -14,6 +15,12 @@ public class MainController {
     @RequestMapping(value = {"/", "index"}, method = RequestMethod.GET)
     public String index(){
         OUT.prt("request:", "index");
+        return "index";
+    }
+
+    @RequestMapping(value = {"/", "index"}, method = RequestMethod.POST)
+    public String indexPost(@RequestPart("picture")byte[] profilePicture){
+        OUT.prt("post:", "index");
         return "index";
     }
 
