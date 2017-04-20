2c55026163fbd21743ef09224b8fdcb14a0ffcad
PanDingrong0129 <1084288424@qq.com>
2017-04-20 14:26:42 +0800

    Add testdatas repository

---
+++ src/main/java/com/xtu/DB/TestdatasRepository.java
@@ -0,0 +1,25 @@
+package com.xtu.DB;
+
+import com.xtu.DB.dto.ProblemsDTO;
+import com.xtu.DB.entity.TestdatasEntity;
+
+import java.util.List;
+
+/**
+ * Created by Ilovezilian on 2017/4/12.
+ */
+
+public interface TestdatasRepository {
+    Long count();
+
+    TestdatasEntity findOne(int problemId);
+
+    TestdatasEntity findOne(int problemId, short no);
+
+    List<TestdatasEntity> find(ProblemsDTO problemsDTO);
+
+    TestdatasEntity save(TestdatasEntity testdatasEntity);
+
+    void delete(long id);
+
+}
