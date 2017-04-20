bf326ded671e1890aac098c488fe9515963d6ff9
PanDingrong0129 <1084288424@qq.com>
2017-04-20 14:24:15 +0800

    Add restrict to TestdatasEntity

--- src/main/java/com/xtu/DB/entity/TestdatasEntity.java
+++ src/main/java/com/xtu/DB/entity/TestdatasEntity.java
@@ -1,7 +1,10 @@
 package com.xtu.DB.entity;
 
+import com.sun.istack.internal.NotNull;
 import lombok.Data;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import java.sql.Timestamp;
 
 /**
@@ -9,12 +12,44 @@ import java.sql.Timestamp;
  */
 @Data
 public class TestdatasEntity {
+    /**
+     * 题目ID
+     */
+    @NotNull
+    @Min(0)
     private int problemId;
+    /**
+     * 测试数据序号
+     */
+    @NotNull
+    @Min(0)
     private short no;
-    private byte score;
+    /**
+     * 分数
+     */
+    @Min(0)
+    @Max(100)
+    private byte score = 0;
+    /**
+     * 输入数据
+     */
     private byte[] input;
+    /**
+     * 输出数据
+     */
     private byte[] output;
+    /**
+     * 建立时间
+     */
     private Timestamp createTime;
+    /**
+     * 最后更新时间
+     */
     private Timestamp lastUpdateTime;
+    /**
+     * 上传者
+     */
+    @NotNull
+    @Min(0)
     private int owner;
 }
