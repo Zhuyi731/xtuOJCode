4f7b3efd4355886bf4136b439fe10b597e0afa78
PanDingrong0129 <1084288424@qq.com>
2017-04-23 21:17:29 +0800

    Deploy security about Oj project

--- src/main/java/com/xtu/config/SecurityWebInitializer.java
+++ src/main/java/com/xtu/config/SecurityWebInitializer.java
@@ -1,8 +1,10 @@
 package com.xtu.config;
 
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
 /**
  * Created by Ilovezilian on 2017/4/18.
  */
-//public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
-//
-//}
+public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
