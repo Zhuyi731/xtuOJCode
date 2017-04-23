05eb166751e577a7817501f709502cae51b4a1db
PanDingrong0129 <1084288424@qq.com>
2017-04-23 21:18:26 +0800

    Add security setting to root config

--- src/main/java/com/xtu/config/RootConfig.java
+++ src/main/java/com/xtu/config/RootConfig.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  * Created by Ilovezilian on 2017/4/11.
  */
 @Configuration
-@Import({WebConfig.class, MysqlConfig.class})
+@Import({WebConfig.class, MysqlConfig.class, SecurityConfig.class})
 //@Import({MysqlConfig.class, WebConfig.class})
 @ComponentScan(basePackages = {"com.xtu.DB"},
 //        @ComponentScan(basePackages = {"com.xtu.DB", "com.xtu.config"},
