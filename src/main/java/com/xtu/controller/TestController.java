37a65fa72379140be9239b4e3c157efde7afc1f7
PanDingrong0129 <1084288424@qq.com>
2017-04-27 15:51:02 +0800

    Add post method of createTest page

--- src/main/java/com/xtu/controller/TestController.java
+++ src/main/java/com/xtu/controller/TestController.java
@@ -1,9 +1,19 @@
 package com.xtu.controller;
 
+import com.xtu.DB.ContestProblemsRepository;
+import com.xtu.DB.ContestRepository;
+import com.xtu.DB.dto.CreateTestDTO;
+import com.xtu.DB.entity.ContestProblemsEntity;
+import com.xtu.DB.entity.ContestsEntity;
 import com.xtu.constant.Pages;
+import com.xtu.tools.DateUtil;
 import com.xtu.tools.OUT;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -13,9 +23,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value = "/" + Pages.TEST)
 public class TestController {
+    @Autowired
+    ContestRepository contestRepository;
+    @Autowired
+    ContestProblemsRepository contestProblemsRepository;
 
-    @RequestMapping(value = "/" + Pages.TEST, method = RequestMethod.GET)
-    public String showTest() {
+    @RequestMapping(value = {"/" + Pages.TEST, "/" + Pages.TEST + "/{id}"}, method = RequestMethod.GET)
+    public String showTest(
+            @PathVariable("id") Integer contestId,
+            Model model) {
         OUT.prt("request", Pages.TEST);
         // TODO: 2017/4/18 select from db
         String res = Pages.TEST + "/" + Pages.TEST;
@@ -31,19 +47,53 @@ public class TestController {
     }
 
     @RequestMapping(value = "/" + Pages.CREATE_TEST, method = RequestMethod.GET)
-    public String createTest(
-//            TODO: CreateEntity entity
+    public String createTest() {
+        OUT.prt("request", Pages.CREATE_TEST);
+        String res = Pages.TEST + "/" + Pages.CREATE_TEST;
+        return res;
+    }
+
+    @Transactional
+    @RequestMapping(value = {"/" + Pages.CREATE_TEST, "/" + Pages.MODIFY_TEST}, method = RequestMethod.POST)
+    public String createTestPost(
+            CreateTestDTO createTestDTO,
+//            @NotNull @Valid CreateTestDTO createTestDTO,
             Model model) {
         OUT.prt("post", Pages.CREATE_TEST);
+        OUT.prt("post", createTestDTO);
+//        createTestDTO.setStartTime(new Timestamp(createTestDTO.getStartTimeStr()));
+//        createTestDTO.setFrozenStartTime(new Timestamp(createTestDTO.getFrozenStartTimeStr()));
+//        createTestDTO.setEndTime(new Timestamp(createTestDTO.getEndTimeStr()));
+        createTestDTO.setStartTime(DateUtil.getTimestamp(createTestDTO.getStartTimeStr()));
+        createTestDTO.setFrozenStartTime(DateUtil.getTimestamp(createTestDTO.getFrozenStartTimeStr()));
+        createTestDTO.setEndTime(DateUtil.getTimestamp(createTestDTO.getEndTimeStr()));
         // TODO: 2017/4/18 insert into db
-        String res = Pages.TEST + "/" + Pages.CREATE_TEST;
+        ContestsEntity contestsEntity = new ContestsEntity();
+        BeanUtils.copyProperties(createTestDTO, contestsEntity);
+
+        contestRepository.save(contestsEntity);
+        ContestProblemsEntity entity = new ContestProblemsEntity();
+        int contestId = contestsEntity.getContestId();
+        entity.setContestId(contestsEntity.getContestId());
+
+        String problems = createTestDTO.getProblemsList();
+        contestProblemsRepository.deleteNoToMax(contestId);
+        for (String s : problems.split("TAT")) {
+            String[] ss = s.split("QAQ");
+            entity.setNo(Byte.parseByte(ss[0]));
+            entity.setProblemId(Integer.parseInt(ss[1]));
+            entity.setScore(Byte.parseByte(ss[2]));
+            contestProblemsRepository.insert(entity);
+        }
+
+        String res = Pages.TEST + "/" + Pages.TOTAL_TEST;
         return res;
     }
 
     @RequestMapping(value = "/" + Pages.JOIN_TEST, method = RequestMethod.GET)
     public String joinTest(
             // TODO: 2017/4/18 create entity
-            Model model){
+            Model model) {
         OUT.prt("request", Pages.JOIN_TEST);
         // TODO: 2017/4/18 insert into db
         String res = Pages.TEST + "/" + Pages.JOIN_TEST;
