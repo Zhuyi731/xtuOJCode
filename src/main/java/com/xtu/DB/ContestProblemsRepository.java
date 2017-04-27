eaf822eadab658a429a64d48e4c2379a14487458
PanDingrong0129 <1084288424@qq.com>
2017-04-27 15:52:01 +0800

    Add contest problems repository

---
+++ src/main/java/com/xtu/DB/ContestProblemsRepository.java
@@ -0,0 +1,29 @@
+package com.xtu.DB;
+
+import com.xtu.DB.dto.ProblemsDTO;
+import com.xtu.DB.entity.ContestProblemsEntity;
+
+import java.util.List;
+
+/**
+ * Created by Ilovezilian on 2017/4/12.
+ */
+
+public interface ContestProblemsRepository {
+    Long count();
+
+    ContestProblemsEntity findOne(String problemId);
+
+    List<ContestProblemsEntity> find(ProblemsDTO problemsDTO);
+
+    ContestProblemsEntity save(ContestProblemsEntity problemSetDetailEntity);
+
+    ContestProblemsEntity insert(ContestProblemsEntity problemSetDetailEntity);
+
+    ContestProblemsEntity update(ContestProblemsEntity problemSetDetailEntity);
+
+    void deleteNoToMax(int contestId);
+
+    void delete(long id);
+
+}
