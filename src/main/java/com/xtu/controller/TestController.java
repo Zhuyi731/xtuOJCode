3a0b9fd0de013bc70908148d3af4cbd8411d8aae
PanDingrong0129 <1084288424@qq.com>
2017-04-28 10:33:21 +0800

    modify showTotalTest and createTestPost method

--- src/main/java/com/xtu/controller/TestController.java
+++ src/main/java/com/xtu/controller/TestController.java
@@ -2,9 +2,14 @@ package com.xtu.controller;
 
 import com.xtu.DB.ContestProblemsRepository;
 import com.xtu.DB.ContestRepository;
+import com.xtu.DB.UsersRepository;
+import com.xtu.DB.dto.ContestDTO;
 import com.xtu.DB.dto.CreateTestDTO;
 import com.xtu.DB.entity.ContestProblemsEntity;
 import com.xtu.DB.entity.ContestsEntity;
+import com.xtu.DB.entity.UsersEntity;
+import com.xtu.DB.vo.AllContestProblemVO;
+import com.xtu.DB.vo.AllContestVO;
 import com.xtu.constant.Pages;
 import com.xtu.tools.DateUtil;
 import com.xtu.tools.OUT;
@@ -17,6 +22,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.security.Principal;
+
 /**
  * Created by Ilovezilian on 2017/4/18.
  */
@@ -27,6 +34,8 @@ public class TestController {
     ContestRepository contestRepository;
     @Autowired
     ContestProblemsRepository contestProblemsRepository;
+    @Autowired
+    UsersRepository usersRepository;
 
     @RequestMapping(value = {"/" + Pages.TEST, "/" + Pages.TEST + "/{id}"}, method = RequestMethod.GET)
     public String showTest(
@@ -34,14 +43,22 @@ public class TestController {
             Model model) {
         OUT.prt("request", Pages.TEST);
         // TODO: 2017/4/18 select from db
+        AllContestProblemVO vo = contestProblemsRepository.findList(contestId);
+        model.addAttribute("vo", vo);
         String res = Pages.TEST + "/" + Pages.TEST;
         return res;
     }
 
-    @RequestMapping(value = "/" + Pages.TOTAL_TEST, method = RequestMethod.GET)
-    public String showTotalTest() {
+    @RequestMapping(value = {"/" + Pages.TOTAL_TEST, "/" + Pages.TOTAL_TEST + "/{start}"}, method = RequestMethod.GET)
+    public String showTotalTest(
+            @PathVariable("start") int start,
+            ContestDTO contestDTO,
+            Model model,
+            Principal principal) {
         OUT.prt("request", Pages.TOTAL_TEST);
-        // TODO: 2017/4/18 select from db
+        contestDTO.setId(principal.getName());
+        AllContestVO vo = contestRepository.queryContestPages(start, contestDTO);
+        model.addAttribute("vo", vo);
         String res = Pages.TEST + "/" + Pages.TOTAL_TEST;
         return res;
     }
@@ -57,8 +74,8 @@ public class TestController {
     @RequestMapping(value = {"/" + Pages.CREATE_TEST, "/" + Pages.MODIFY_TEST}, method = RequestMethod.POST)
     public String createTestPost(
             CreateTestDTO createTestDTO,
-//            @NotNull @Valid CreateTestDTO createTestDTO,
-            Model model) {
+            Model model,
+            Principal principal) {
         OUT.prt("post", Pages.CREATE_TEST);
         OUT.prt("post", createTestDTO);
 //        createTestDTO.setStartTime(new Timestamp(createTestDTO.getStartTimeStr()));
@@ -71,15 +88,22 @@ public class TestController {
         ContestsEntity contestsEntity = new ContestsEntity();
         BeanUtils.copyProperties(createTestDTO, contestsEntity);
 
+        /**
+         * 获取user_id
+         */
+        String id = principal.getName();
+        UsersEntity usersEntity = usersRepository.findOne(id);
+        contestsEntity.setOwner(usersEntity.getUserId());
+
         contestRepository.save(contestsEntity);
         ContestProblemsEntity entity = new ContestProblemsEntity();
         int contestId = contestsEntity.getContestId();
         entity.setContestId(contestsEntity.getContestId());
 
-        String problems = createTestDTO.getProblemsList();
+        String problems = createTestDTO.getProblemList();
         contestProblemsRepository.deleteNoToMax(contestId);
-        for (String s : problems.split("TAT")) {
-            String[] ss = s.split("QAQ");
+        for (String s : problems.split("QAQ")) {
+            String[] ss = s.split("TAT");
             entity.setNo(Byte.parseByte(ss[0]));
             entity.setProblemId(Integer.parseInt(ss[1]));
             entity.setScore(Byte.parseByte(ss[2]));
