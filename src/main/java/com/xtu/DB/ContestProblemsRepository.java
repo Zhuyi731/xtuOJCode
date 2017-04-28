0a7d0712dfe50fdafa40bb33afc0f87ef90e3ee5
PanDingrong0129 <1084288424@qq.com>
2017-04-28 10:21:36 +0800

    Add the interface of find list

--- src/main/java/com/xtu/DB/ContestProblemsRepository.java
+++ src/main/java/com/xtu/DB/ContestProblemsRepository.java
@@ -2,6 +2,7 @@ package com.xtu.DB;
 
 import com.xtu.DB.dto.ProblemsDTO;
 import com.xtu.DB.entity.ContestProblemsEntity;
+import com.xtu.DB.vo.AllContestProblemVO;
 
 import java.util.List;
 
@@ -15,6 +16,8 @@ public interface ContestProblemsRepository {
     ContestProblemsEntity findOne(String problemId);
 
     List<ContestProblemsEntity> find(ProblemsDTO problemsDTO);
+
+    AllContestProblemVO findList(int contestId);
 
     ContestProblemsEntity save(ContestProblemsEntity problemSetDetailEntity);
 
