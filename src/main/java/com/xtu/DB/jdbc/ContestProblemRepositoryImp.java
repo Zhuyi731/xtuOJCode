7ecda8e69e38703df5a3b8a3efa2e1cf523d1d8d
PanDingrong0129 <1084288424@qq.com>
2017-04-28 10:20:56 +0800

    Add query list of contest problem method

--- src/main/java/com/xtu/DB/jdbc/ContestProblemRepositoryImp.java
+++ src/main/java/com/xtu/DB/jdbc/ContestProblemRepositoryImp.java
@@ -1,13 +1,21 @@
 package com.xtu.DB.jdbc;
 
 import com.xtu.DB.ContestProblemsRepository;
+import com.xtu.DB.ProblemsRepository;
 import com.xtu.DB.dto.ProblemsDTO;
 import com.xtu.DB.entity.ContestProblemsEntity;
+import com.xtu.DB.entity.ProblemsEntity;
+import com.xtu.DB.vo.AllContestProblemEntityVO;
+import com.xtu.DB.vo.AllContestProblemVO;
 import com.xtu.constant.Tables;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -17,6 +25,8 @@ import java.util.List;
 public class ContestProblemRepositoryImp implements ContestProblemsRepository {
     @Autowired
     JdbcOperations jdbcOperations;
+    @Autowired
+    ProblemsRepository problemsRepository;
 
     @Override
     public Long count() {
@@ -31,6 +41,28 @@ public class ContestProblemRepositoryImp implements ContestProblemsRepository {
     @Override
     public List<ContestProblemsEntity> find(ProblemsDTO problemsDTO) {
         return null;
+    }
+
+    @Override
+    public AllContestProblemVO findList(int contestId) {
+        String sql = " SELECT * FROM "
+                + Tables.CONTEST_PROBLEMS
+                + " WHERE `contest_id` = ?";
+        List<ContestProblemsEntity> entityList =
+                jdbcOperations.query(sql, new ContestProblemsEntityRowMapper(), contestId);
+        AllContestProblemVO vo = new AllContestProblemVO();
+        List<AllContestProblemEntityVO> entityVO = vo.getEntityList();
+        for (ContestProblemsEntity entity : entityList) {
+            AllContestProblemEntityVO problemEntityVO = new AllContestProblemEntityVO();
+            BeanUtils.copyProperties(entity, problemEntityVO);
+
+            ProblemsEntity problemsEntity = problemsRepository.queryOne(entity.getProblemId());
+            problemEntityVO.setTitle(problemsEntity.getTitle());
+            entityVO.add(problemEntityVO);
+        }
+        vo.setStart(0);
+        vo.setTotal(entityList.size());
+        return vo;
     }
 
     @Override
@@ -84,4 +116,16 @@ public class ContestProblemRepositoryImp implements ContestProblemsRepository {
 
     }
 
+    private static final class ContestProblemsEntityRowMapper implements RowMapper<ContestProblemsEntity> {
+        @Override
+        public ContestProblemsEntity mapRow(ResultSet rs, int rowNum) throws SQLException {
+            ContestProblemsEntity entity = new ContestProblemsEntity();
+            entity.setContestId(rs.getInt("contest_id"));
+            entity.setNo(rs.getByte("no"));
+            entity.setProblemId(rs.getInt("problem_id"));
+            entity.setScore(rs.getByte("score"));
+            entity.setBalloonColor(rs.getString("balloon_color"));
+            return entity;
+        }
+    }
 }
