17ca31919735bb640c315363bee00c5d9021b5bc
PanDingrong0129 <1084288424@qq.com>
2017-04-28 10:35:02 +0800

    Add all contest problem entity vo

---
+++ src/main/java/com/xtu/DB/vo/AllContestProblemEntityVO.java
@@ -0,0 +1,34 @@
+package com.xtu.DB.vo;
+
+import lombok.Data;
+
+/**
+ * Created by Ilovezilian on 2017/4/27.
+ */
+@Data
+public class AllContestProblemEntityVO {
+    /**
+     * 比赛ID
+     */
+    private int contestId;
+    /**
+     * 比赛题目序号
+     */
+    private byte no;
+    /**
+     * 题目ID
+     */
+    private int problemId;
+    /**
+     * 题目名称
+     */
+    private String title;
+    /**
+     * 题目分值
+     */
+    private short score;
+    /**
+     * 气球颜色，格式为22FF33
+     */
+    private String balloonColor;
+}
