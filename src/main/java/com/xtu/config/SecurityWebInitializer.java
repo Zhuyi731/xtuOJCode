f62e96644481839ab1c381fe4ad6c65b22ca428f
PanDingrong0129 <1084288424@qq.com>
2017-04-29 18:37:31 +0800

    ADd encoding of utf-8 to spring security initailizer

--- src/main/java/com/xtu/config/SecurityWebInitializer.java
+++ src/main/java/com/xtu/config/SecurityWebInitializer.java
@@ -1,6 +1,10 @@
 package com.xtu.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
 
 /**
  * 配置spring security 初始化
@@ -8,4 +12,11 @@ import org.springframework.security.web.context.AbstractSecurityWebApplicationIn
  */
 public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
 
+    @Override
+    protected void beforeSpringSecurityFilterChain(ServletContext servletContext) {
+        FilterRegistration.Dynamic characterEncodingFilter = servletContext.addFilter("encodingFilter", new CharacterEncodingFilter());
+        characterEncodingFilter.setInitParameter("encoding", "UTF-8");
+        characterEncodingFilter.setInitParameter("forceEncoding", "true");
+        characterEncodingFilter.addMappingForUrlPatterns(null, false, "/*");
+    }
 }
