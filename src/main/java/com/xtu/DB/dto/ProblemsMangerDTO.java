366f3891da39e3ca08aab61c3f58a2035b05e500
PanDingrong0129 <1084288424@qq.com>
2017-04-30 18:12:37 +0800

    Add problems manager dto

---
+++ src/main/java/com/xtu/DB/dto/ProblemsMangerDTO.java
@@ -0,0 +1,34 @@
+package com.xtu.DB.dto;
+
+import lombok.Data;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+/**
+ * Created by Ilovezilian on 2017/4/30.
+ */
+@Data
+public class ProblemsMangerDTO {
+    /**
+     * 题目id
+     */
+    @Min(0)
+    private int problemId = 0;
+    /**
+     * 题目名称
+     */
+    @Size(max = 50)
+    private String title = "";
+    /**
+     * 作者，同users.Id
+     */
+    @Size(max = 50)
+    private String author;
+
+    /**
+     * 拥有者
+     */
+    @Min(0)
+    private int owner = 0;
+}
