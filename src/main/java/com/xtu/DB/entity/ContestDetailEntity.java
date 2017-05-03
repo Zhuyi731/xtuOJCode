7ce13ef6802edcc3f6c291fa5ba44a8debf77b4c
PanDingrong0129 <1084288424@qq.com>
2017-05-03 13:49:54 +0800

    Add remark to contest detail entity

--- src/main/java/com/xtu/DB/entity/ContestDetailEntity.java
+++ src/main/java/com/xtu/DB/entity/ContestDetailEntity.java
@@ -7,13 +7,40 @@ import lombok.Data;
  */
 @Data
 public class ContestDetailEntity {
+    /**
+     * 用户ID
+     */
     private int userId;
+    /**
+     * 比赛ID
+     */
     private int contestId;
+    /**
+     * 比赛题目序号
+     */
     private byte no;
+    /**
+     * 最早AC时间，为NULL表示没有AC
+     */
     private int acceptedTime;
+    /**
+     * 罚时次数
+     */
     private short penaltyTimes;
+    /**
+     * 得分
+     */
     private short score;
+    /**
+     * FB标志，0否1是
+     */
     private byte firstBlood;
+    /**
+     * 封板阶段提交次数
+     */
     private byte frozenSubmitTimes;
+    /**
+     * 气球发放标志(0否1是）
+     */
     private byte balloon;
 }
