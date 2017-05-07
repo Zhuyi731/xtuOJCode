82b88a183ee8d2f5fd6b5e157bb0604b64510a96
PanDingrong0129 <1084288424@qq.com>
2017-05-07 10:01:05 +0800

    Add admin manage of users about show users and create new users

--- src/main/java/com/xtu/controller/AdminController.java
+++ src/main/java/com/xtu/controller/AdminController.java
@@ -1,10 +1,19 @@
 package com.xtu.controller;
 
+import com.xtu.DB.UsersRepository;
+import com.xtu.DB.dto.UsersDTO;
+import com.xtu.DB.entity.UsersEntity;
 import com.xtu.constant.Pages;
 import com.xtu.tools.OUT;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+import java.util.List;
 
 /**
  * 管理员相关页面控制器
@@ -13,6 +22,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value = "/" + Pages.ADMIN)
 public class AdminController {
+    @Autowired
+    UsersRepository usersRepository;
 
     /**
      * 主界面框架
@@ -47,6 +58,33 @@ public class AdminController {
     public String modifyPassword() {
         OUT.prt("request", Pages.MODIFY_PASSWORD);
         String res = Pages.MODIFY_PASSWORD;
+        return res;
+    }
+
+    /**
+     * 显示所有用户
+     */
+    @RequestMapping(value = {"/" + Pages.SHOW_USERS_PAGE, "/" + Pages.SHOW_USERS_PAGE + "/{start}"})
+    public String showUsers(
+            @PathVariable("start") int start,
+            @Valid UsersDTO dto,
+            Model model) {
+        OUT.prt("request", Pages.SHOW_USERS_PAGE);
+        OUT.prt("UsersDTO", dto);
+        String res = "/" + Pages.ADMIN + "/" + Pages.SHOW_USERS_PAGE;
+        List<UsersEntity> usersEntityList = usersRepository.queryList(start, dto);
+
+        model.addAttribute("vo", usersEntityList);
+        OUT.prt("vo", usersEntityList);
+        return res;
+    }
+
+    /**
+     * 添加用户
+     */
+    @RequestMapping(value = "/" + Pages.CREATE_NEW_USERS, method = RequestMethod.GET)
+    public String createNewUsers() {
+        String res = "/" + Pages.ADMIN + "/" + Pages.CREATE_NEW_USERS;
         return res;
     }
 }
